463181816b2c6a0f74e02705cf9f801f421ab07d
Jeff Disher <jeff@jeffdisher.com>
2022-02-27 21:06:18 -0500

    Do not generate the headers in generated files

--- src/generate_data.sh
+++ src/generate_data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-xjc -p com.jeffdisher.cacophony.data.global.records ../xsd/global/records.xsd
-xjc -p com.jeffdisher.cacophony.data.global.record ../xsd/global/record.xsd
-xjc -p com.jeffdisher.cacophony.data.global.description ../xsd/global/description.xsd
-xjc -p com.jeffdisher.cacophony.data.global.index ../xsd/global/index.xsd
-xjc -p com.jeffdisher.cacophony.data.global.recommendations ../xsd/global/recommendations.xsd
+xjc -no-header -p com.jeffdisher.cacophony.data.global.records ../xsd/global/records.xsd
+xjc -no-header -p com.jeffdisher.cacophony.data.global.record ../xsd/global/record.xsd
+xjc -no-header -p com.jeffdisher.cacophony.data.global.description ../xsd/global/description.xsd
+xjc -no-header -p com.jeffdisher.cacophony.data.global.index ../xsd/global/index.xsd
+xjc -no-header -p com.jeffdisher.cacophony.data.global.recommendations ../xsd/global/recommendations.xsd
