4f1046fc006e23291fb396b49c0c16b55942f7bf
Jeff Disher <jeff@jeffdisher.com>
2022-12-29 01:52:24 -0500

    Quality of life for the video config
    -made the FPS optional, defaulting to not specified
    -changed the default mode to 480p at 30 FPS, since that seems to be more universal
    -added convenient presets for 480p and 720p, since those are the most common options
    -the video preview is now only shown when the camera is on (avoids a big white space in the page)

--- src/com/jeffdisher/cacophony/interactive/GET_VideoConfig.java
+++ src/com/jeffdisher/cacophony/interactive/GET_VideoConfig.java
@@ -13,6 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
  * Returns a JSON struct of the video config options:
  * -height
  * -width
+ * -enableFps
  * -fps
  * -videoBitrate
  * -audioBitrate
@@ -41,9 +42,11 @@ public class GET_VideoConfig implements IGetHandler
 			response.setStatus(HttpServletResponse.SC_OK);
 			
 			JsonObject object = new JsonObject();
-			object.set("height", 720);
-			object.set("width", 1280);
-			object.set("fps", 15);
+			// We will default to 480 with default FPS since this resolution is commonly supported but the framerate is finicky.
+			object.set("height", 480);
+			object.set("width", 640);
+			object.set("enableFps", false);
+			object.set("fps", 30);
 			// The browser tends to default to something like 2.5 Mbps for video and 128 kbps for audio but we will use a smaller number in case the user can't post-process (they can change this in the UI, themselves).
 			object.set("videoBitrate", 256000);
 			object.set("audioBitrate", 64000);
