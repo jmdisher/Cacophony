55ed826c6930946bf23128e435a2f65825d2cd2d
Jeff Disher <jeff@jeffdisher.com>
2023-05-16 22:25:37 -0400

    Added more documentation around the distinction between these cache modes
    -the followee cache element doesn't include the size of the StreamRecord since it always caches all StreamRecords
    -the other cached record info includes the size of all referenced data, including the StreamRecord, since each element is explicitly managed
    -in the future, the followee cache management strategy may change, allowing CachedRecordInfo to be used, but that would change much of the function of that cache, in terms of the decentralized data model, so there are no immediate plans to make such a change

--- src/com/jeffdisher/cacophony/data/local/v1/FollowingCacheElement.java
+++ src/com/jeffdisher/cacophony/data/local/v1/FollowingCacheElement.java
@@ -8,6 +8,10 @@ import com.jeffdisher.cacophony.types.IpfsFile;
  * The hash of the element is stored as well as the special image hash (can be null) and the leaf data node.
  * The leaf node refers to the specific file actually hashed since there are often multiple options but a given follower
  * should only cache the one they care about.
+ * NOTE:  combinedSizeBytes includes only the size of the data pointed to by imageHash and leafHash, NOT elementHash.
+ * This is due to the design of the followee cache ALWAYS caching all meta-data (hence the StreamRecord referenced by
+ * elementHash), so only the leaf data is counted toward the followee cache occupancy.
+ * This is an important difference as compared to CachedRecordInfo.
  */
 public record FollowingCacheElement(IpfsFile elementHash, IpfsFile imageHash, IpfsFile leafHash, long combinedSizeBytes)
 {
--- src/com/jeffdisher/cacophony/projection/CachedRecordInfo.java
+++ src/com/jeffdisher/cacophony/projection/CachedRecordInfo.java
@@ -3,6 +3,18 @@ package com.jeffdisher.cacophony.projection;
 import com.jeffdisher.cacophony.types.IpfsFile;
 
 
+/**
+ * The representation of cached StreamRecord instances in most miscellaneous caching cases.
+ * Details:
+ * -streamCid points to the StreamRecord and cannot be null
+ * -thumbnailCid points to the special record thumbnail (can be null)
+ * -videoCid points to the chosen video cached (can be null)
+ * -audioCid points to the audio cached (can be null)
+ * -at most, ONE of videoCid and audioCid can be set
+ * -combinedSizeBytes includes the size of ALL of these elements:  streamCid, thumbnailCid, videoCid, and audioCid
+ * NOTE:  This is NOT used by followee cache elements since it accounts for element size differently.  See
+ * FollowingCacheElement for more information.
+ */
 public record CachedRecordInfo(IpfsFile streamCid, IpfsFile thumbnailCid, IpfsFile videoCid, IpfsFile audioCid, long combinedSizeBytes)
 {
 }
