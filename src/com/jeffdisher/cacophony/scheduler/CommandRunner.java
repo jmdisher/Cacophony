ecca595c432bb34e6224f5687e32d3e3e4744c84
Jeff Disher <jeff@jeffdisher.com>
2023-05-19 21:13:20 -0400

    Added the ability to schedule commands with blocking dependencies
    -this will be used in later changes to ensure that we don't concurrently try to modify the data rooted at a given key (home user or followee)

--- src/com/jeffdisher/cacophony/scheduler/CommandRunner.java
+++ src/com/jeffdisher/cacophony/scheduler/CommandRunner.java
@@ -1,8 +1,14 @@
 package com.jeffdisher.cacophony.scheduler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.jeffdisher.cacophony.commands.Context;
 import com.jeffdisher.cacophony.commands.ICommand;
 import com.jeffdisher.cacophony.types.CacophonyException;
+import com.jeffdisher.cacophony.types.IpfsKey;
 import com.jeffdisher.cacophony.utils.Assert;
 import com.jeffdisher.cacophony.utils.MiscHelpers;
 
@@ -11,12 +17,15 @@ import com.jeffdisher.cacophony.utils.MiscHelpers;
  * Runs commands on background threads, giving each their own clone of the shared context to avoid pollution between
  * commands.
  * Returns a future which exposes the result of the command and its context.
+ * The commands are scheduled and started in the order they are presented, except in the case of runBlockedCommand where
+ * a command will not be scheduled until no other scheduled or running commands share the same key.
  */
 public class CommandRunner
 {
 	private final Context _sharedContext;
 	private final WorkQueue _queue;
 	private final Thread[] _threads;
+	private final Map<IpfsKey, List<Runnable>> _blockedRunnables;
 
 	/**
 	 * Creates a runner in a not-started state.
@@ -47,6 +56,7 @@ public class CommandRunner
 				}
 			}, "CommandRunner thread #" + i);
 		}
+		_blockedRunnables = new HashMap<>();
 	}
 
 	/**
@@ -108,5 +118,75 @@ public class CommandRunner
 			}
 		});
 		return future;
+	}
+
+	/**
+	 * Similar to runCommand but will not enqueue the command to run until there are no commands running or in the queue
+	 * which share the same blockingKey.
+	 * 
+	 * @param <T> The return type of the command.
+	 * @param blockingKey The key which this command will treat as a mutex around scheduling and execution.
+	 * @param command The command to run.
+	 * @return A future describing the command result/error and context where it was executed.
+	 */
+	public <T extends ICommand.Result> FutureCommand<T> runBlockedCommand(IpfsKey blockingKey, ICommand<T> command)
+	{
+		FutureCommand<T> future = new FutureCommand<>();
+		Runnable runnable = () -> {
+			Context one = _sharedContext.cloneWithSelectedKey(_sharedContext.keyName);
+			try
+			{
+				future.setContext(one);
+				T result = command.runInContext(one);
+				future.success(result);
+				// We don't expect this instance to change with the commands currently run in interactive mode.
+				Assert.assertTrue(_sharedContext.getSelectedKey() == one.getSelectedKey());
+			}
+			catch (CacophonyException e)
+			{
+				future.failure(e);
+			}
+			finally
+			{
+				_unblockKey(blockingKey);
+			}
+		};
+		_enqueueOrBlock(blockingKey, runnable);
+		return future;
+	}
+
+
+	private synchronized void _enqueueOrBlock(IpfsKey blockingKey, Runnable runnable)
+	{
+		List<Runnable> runnables = _blockedRunnables.get(blockingKey);
+		if (null == runnables)
+		{
+			// There is nothing in the queue with this key so we can add the empty blocking queue and enqueue this to the scheduler.
+			_blockedRunnables.put(blockingKey, new ArrayList<>());
+			_queue.enqueue(runnable);
+		}
+		else
+		{
+			// We are blocked on someone so just enter the blocking queue.
+			runnables.add(runnable);
+		}
+	}
+
+	private synchronized void _unblockKey(IpfsKey blockingKey)
+	{
+		List<Runnable> runnables = _blockedRunnables.get(blockingKey);
+		// Since we were running and blocking this queue, this MUST not be null.
+		Assert.assertTrue(null != runnables);
+		
+		// If the queue is empty, just remove it.  Otherwise, enqueue the next element in the work queue.
+		if (runnables.isEmpty())
+		{
+			_blockedRunnables.remove(blockingKey);
+		}
+		else
+		{
+			Runnable next = runnables.remove(0);
+			_queue.enqueue(next);
+		}
 	}
 }
--- test/com/jeffdisher/cacophony/scheduler/TestCommandRunner.java
+++ test/com/jeffdisher/cacophony/scheduler/TestCommandRunner.java
@@ -11,10 +11,14 @@ import com.jeffdisher.cacophony.commands.Context;
 import com.jeffdisher.cacophony.commands.ICommand;
 import com.jeffdisher.cacophony.commands.results.None;
 import com.jeffdisher.cacophony.types.CacophonyException;
+import com.jeffdisher.cacophony.types.IpfsKey;
 
 
 public class TestCommandRunner
 {
+	public static final IpfsKey K1 = IpfsKey.fromPublicKey("z5AanNVJCxnSSsLjo4tuHNWSmYs3TXBgKWxVqdyNFgwb1br5PBWo14F");
+	public static final IpfsKey K2 = IpfsKey.fromPublicKey("z5AanNVJCxnSSsLjo4tuHNWSmYs3TXBgKWxVqdyNFgwb1br5PBWo14W");
+
 	@Test
 	public void basicPass() throws Throwable
 	{
@@ -87,6 +91,58 @@ public class TestCommandRunner
 		runner.shutdownThreads();
 	}
 
+	@Test
+	public void blockingCase() throws Throwable
+	{
+		Context context = _buildContext();
+		CommandRunner runner = new CommandRunner(context, 2);
+		
+		// We will use a pointer to a shared counter and enqueue some commands before the start the runner.
+		// We expect to see the commands observe the numbers in the counters in the correct order.
+		// We will also throw in some barriers and non-blocking commands to synthesize back-pressure to make out-of-order execution more likely, if the blocking system isn't working.
+		// NOTE:  If there is a problem here, it will likely not be observed 100% of the time, but this order interaction should make issues pretty likely.
+		int count1[] = new int[1];
+		int count2[] = new int[1];
+		CyclicBarrier barrier = new CyclicBarrier(3);
+		CountingCommand count1_1 = new CountingCommand(count1, null);
+		CountingCommand count1_2 = new CountingCommand(count1, null);
+		CountingCommand count1_3 = new CountingCommand(count1, barrier);
+		TestCommand blank1 = new TestCommand(true, null);
+		CountingCommand count2_1 = new CountingCommand(count2, barrier);
+		CountingCommand count2_2 = new CountingCommand(count2, null);
+		CountingCommand count2_3 = new CountingCommand(count2, null);
+		TestCommand blank2 = new TestCommand(true, null);
+		
+		FutureCommand<None> f1_1 = runner.runBlockedCommand(K1, count1_1);
+		FutureCommand<None> f1_2 = runner.runBlockedCommand(K1, count1_2);
+		FutureCommand<None> f1_3 = runner.runBlockedCommand(K1, count1_3);
+		FutureCommand<None> fb1 = runner.runCommand(blank1);
+		FutureCommand<None> f2_1 = runner.runBlockedCommand(K2, count2_1);
+		FutureCommand<None> f2_2 = runner.runBlockedCommand(K2, count2_2);
+		FutureCommand<None> f2_3 = runner.runBlockedCommand(K2, count2_3);
+		FutureCommand<None> fb2 = runner.runCommand(blank2);
+		runner.startThreads();
+		
+		// This order is a valid order within the implementation but may change if the implementation changes.
+		f1_1.get();
+		fb1.get();
+		fb2.get();
+		f1_2.get();
+		barrier.await();
+		f1_3.get();
+		f2_1.get();
+		f2_2.get();
+		f2_3.get();
+		
+		// We verify that everything ran in the correct order by looking at the counter they observed (the blocking semantics should avoid any races or concurrent access).
+		Assert.assertEquals(0, count1_1.observedValue);
+		Assert.assertEquals(1, count1_2.observedValue);
+		Assert.assertEquals(2, count1_3.observedValue);
+		Assert.assertEquals(0, count2_1.observedValue);
+		Assert.assertEquals(1, count2_2.observedValue);
+		Assert.assertEquals(2, count2_3.observedValue);
+	}
+
 
 	private static Context _buildContext()
 	{
@@ -140,6 +196,40 @@ public class TestCommandRunner
 			{
 				throw new CacophonyException("TEST");
 			}
+		}
+	}
+
+	private static class CountingCommand implements ICommand<None>
+	{
+		private final int[] _counterRef;
+		private final CyclicBarrier _barrier;
+		public int observedValue;
+		public CountingCommand(int[] counterRef, CyclicBarrier barrier)
+		{
+			_counterRef = counterRef;
+			_barrier = barrier;
+		}
+		@Override
+		public None runInContext(Context context) throws CacophonyException
+		{
+			if (null != _barrier)
+			{
+				try
+				{
+					_barrier.await();
+				}
+				catch (InterruptedException e)
+				{
+					throw new AssertionError(e);
+				}
+				catch (BrokenBarrierException e)
+				{
+					throw new AssertionError(e);
+				}
+			}
+			this.observedValue = _counterRef[0];
+			_counterRef[0] = this.observedValue + 1;
+			return None.NONE;
 		}
 	}
 }
