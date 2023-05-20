5d9824495f8ceef07910414b88d6611a63d59c78
Jeff Disher <jeff@jeffdisher.com>
2023-05-20 00:51:25 -0400

    Return 404 on no public key
    -this needs to be further fleshed out in later front-end changes for multi-channel but this was low-hanging fruit encountered during prototyping

--- src/com/jeffdisher/cacophony/interactive/GET_PublicKey.java
+++ src/com/jeffdisher/cacophony/interactive/GET_PublicKey.java
@@ -1,6 +1,7 @@
 package com.jeffdisher.cacophony.interactive;
 
 import com.jeffdisher.cacophony.commands.Context;
+import com.jeffdisher.cacophony.types.IpfsKey;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -23,7 +24,15 @@ public class GET_PublicKey implements ValidatedEntryPoints.GET
 	public void handle(HttpServletRequest request, HttpServletResponse response, String[] variables) throws Throwable
 	{
 		response.setContentType("text/plain");
-		response.setStatus(HttpServletResponse.SC_OK);
-		response.getWriter().print(_context.publicKey.toPublicKey());
+		IpfsKey key = _context.publicKey;
+		if (null != key)
+		{
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().print(key.toPublicKey());
+		}
+		else
+		{
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
 	}
 }
