f54d7c2b527ad2a545697068636801c0f41ef741
Jeff Disher <jeff@jeffdisher.com>
2023-05-23 22:34:19 -0400

    Handle error cases better in GetPublicKeyCommand

--- src/com/jeffdisher/cacophony/commands/GetPublicKeyCommand.java
+++ src/com/jeffdisher/cacophony/commands/GetPublicKeyCommand.java
@@ -3,15 +3,22 @@ package com.jeffdisher.cacophony.commands;
 import com.jeffdisher.cacophony.commands.results.None;
 import com.jeffdisher.cacophony.logic.ILogger;
 import com.jeffdisher.cacophony.types.IpfsConnectionException;
+import com.jeffdisher.cacophony.types.IpfsKey;
+import com.jeffdisher.cacophony.types.UsageException;
 
 
 public record GetPublicKeyCommand() implements ICommand<None>
 {
 	@Override
-	public None runInContext(Context context) throws IpfsConnectionException
+	public None runInContext(Context context) throws IpfsConnectionException, UsageException
 	{
+		IpfsKey publicKey = context.getSelectedKey();
+		if (null == publicKey)
+		{
+			throw new UsageException("Channel must first be created with --createNewChannel");
+		}
 		ILogger log = context.logger.logStart("Public Key:");
-		log.logOperation("Public Key (other users can follow you with this): " + context.getSelectedKey().toPublicKey());
+		log.logOperation("Public Key (other users can follow you with this): " + publicKey.toPublicKey());
 		log.logFinish("");
 		return None.NONE;
 	}
