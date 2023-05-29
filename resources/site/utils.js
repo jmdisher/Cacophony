aa751b6521211f44e096d8995d3c723a3ca8db76
Jeff Disher <jeff@jeffdisher.com>
2023-05-29 19:12:48 -0400

    Coalesced the common post loading helper
    -this pattern is common across a few pages so this formalizes it

--- resources/site/utils.js
+++ resources/site/utils.js
@@ -15,3 +15,59 @@ function readGetVar(name)
 	return vars[name];
 }
 
+// Adds a placeholder for the "hash" StreamRecord into "array", adding to beginning if "isNewest" or the end.
+// Asynchronously starts loading the actual information for the StreamRecord, calling "updateCallback" when returned.
+// Keys in the element added to the array:
+// "cached"
+// "elementHash"
+// "name"
+// "description"
+// "readableData"
+// "isDeleting"
+// "thumbnailUrl"
+function addElementHashToArray(array, hash, isNewest, updateCallback)
+{
+	// Add empty objects to the array since we will replace them asynchronously.
+	let placeholder = {
+		"cached": false,
+		"elementHash": hash,
+		"name": "Loading...",
+	};
+	// We want to build this backward so that the most recent additions are at the top of the screen.
+	if (isNewest)
+	{
+		array.unshift(placeholder);
+	}
+	else
+	{
+		array.push(placeholder);
+	}
+	API_getPost(hash).then(elt => {
+		// We want to make sure the description isn't too long to reasonably render (since it is allowed to be unbounded in length, at the protocol level).
+		let description = elt["description"];
+		if (description.length > 135)
+		{
+			// We use mismatched truncation to avoid spilling just a few chars - the actual limits are unimportant.
+			description = description.slice(0, 130) + "...";
+		}
+		// We can't update the placeholder since the $apply() won't see nested updates so replace this.
+		let object = {
+			"cached": elt["cached"],
+			"elementHash": hash,
+			"readableDate": new Date(elt["publishedSecondsUtc"] * 1000).toLocaleString(),
+			"name": elt["name"],
+			"description": description,
+			"isDeleting": false,
+		}
+		if (elt["cached"])
+		{
+			object["thumbnailUrl"] = elt["thumbnailUrl"];
+		}
+		// We need to replace the index due to the earlier mention of $apply.
+		// (we will use indexOf() since this is an exact instance match)
+		let index = array.indexOf(placeholder);
+		array[index] = object;
+		updateCallback();
+	});
+}
+
