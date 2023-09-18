jmdisher/Cacophony

commit 5106a246e440886213bcb43aefbc2415eb1df0b5

ExplicitCacheManager is now final in Context
-now that the cyclic dependency is resolved, these can be initialized and connected together in a more concrete order

src/com/jeffdisher/cacophony/commands/Context.java
@@ -14,7 +14,6 @@ import com.jeffdisher.cacophony.logic.IConnection;
 import com.jeffdisher.cacophony.logic.ILogger;
 import com.jeffdisher.cacophony.scheduler.INetworkScheduler;
 import com.jeffdisher.cacophony.types.IpfsKey;
-import com.jeffdisher.cacophony.utils.Assert;
 
 
 /**
@@ -33,8 +32,8 @@ public class Context
 	public final ILocalUserInfoCache userInfoCache;
 	public final IEntryCacheRegistry entryRegistry;
 	public final CacheUpdater cacheUpdater;
+	public final ExplicitCacheManager explicitCacheManager;
 	private IpfsKey _selectedKey;
-	private ExplicitCacheManager _explicitCache;
 
 	public Context(DraftManager sharedDraftManager
 			, AccessTuple accessTuple
@@ -45,6 +44,7 @@ public class Context
 			, ILocalUserInfoCache userInfoCache
 			, IEntryCacheRegistry entryRegistry
 			, CacheUpdater cacheUpdater
+			, ExplicitCacheManager explicitCacheManager
 			, IpfsKey selectedKey
 	)
 	{
@@ -57,6 +57,7 @@ public class Context
 		this.userInfoCache = userInfoCache;
 		this.entryRegistry = entryRegistry;
 		this.cacheUpdater = cacheUpdater;
+		this.explicitCacheManager = explicitCacheManager;
 		_selectedKey = selectedKey;
 	}
 
@@ -71,18 +72,6 @@ public class Context
 		_selectedKey = key;
 	}
 
-	public ExplicitCacheManager getExplicitCache()
-	{
-		// If we request this, it must have been configured already.
-		Assert.assertTrue(null != _explicitCache);
-		return _explicitCache;
-	}
-
-	public void setExplicitCache(ExplicitCacheManager explicitCache)
-	{
-		_explicitCache = explicitCache;
-	}
-
 	public Context cloneWithSelectedKey(IpfsKey selectedKey)
 	{
 		// We reference everything as a shared structure except for the key-name map, which is a duplicate.
@@ -95,9 +84,9 @@ public class Context
 				, this.userInfoCache
 				, this.entryRegistry
 				, this.cacheUpdater
+				, this.explicitCacheManager
 				, selectedKey
 		);
-		context.setExplicitCache(_explicitCache);
 		return context;
 	}
 
@@ -118,9 +107,9 @@ public class Context
 				, userInfoCache
 				, entryRegistry
 				, cacheUpdater
+				, this.explicitCacheManager
 				, _selectedKey
 		);
-		context.setExplicitCache(explicitCacheManager);
 		return context;
 	}
 

src/com/jeffdisher/cacophony/Cacophony.java
@@ -3,6 +3,7 @@ package com.jeffdisher.cacophony;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URL;
+import java.util.function.LongSupplier;
 
 import com.jeffdisher.cacophony.access.IReadingAccess;
 import com.jeffdisher.cacophony.access.StandardAccess;
@@ -129,19 +130,19 @@ public class Cacophony {
 					boolean verbose = (null != System.getenv(EnvVars.ENV_VAR_CACOPHONY_VERBOSE));
 					StandardLogger logger = StandardLogger.topLogger(System.out, verbose);
 					URL baseUrl = connectionData.second();
-					Context context = _createContext(keyName
-							, localDataModel
-							, draftManager
-							, connection
-							, scheduler
-							, logger
-							, baseUrl
-					);
-					
+					Context.AccessTuple accessTuple = new Context.AccessTuple(localDataModel, connection, scheduler);
+					LongSupplier currentTimeMillisGenerator = () -> System.currentTimeMillis();
 					// Create the default explicit cache manager (could be over-ridden by the interactive server, in its Context).
 					// The default manager runs in synchronous mode, since that makes more sense for command-line usage.
-					ExplicitCacheManager explicitCacheManager = new ExplicitCacheManager(context.accessTuple, context.logger, context.currentTimeMillisGenerator, false);
-					context.setExplicitCache(explicitCacheManager);
+					ExplicitCacheManager explicitCacheManager = new ExplicitCacheManager(accessTuple, logger, currentTimeMillisGenerator, false);
+					Context context = _createContext(keyName
+							, draftManager
+							, accessTuple
+							, logger
+							, currentTimeMillisGenerator
+							, explicitCacheManager
+							, baseUrl
+					);
 					
 					// Now, run the actual command (this normally returns soon but commands could be very long-running).
 					ICommand.Result result = command.runInContext(context);
@@ -209,17 +210,17 @@ public class Cacophony {
 	}
 
 	private static Context _createContext(String keyName
-			, LocalDataModel localDataModel
 			, DraftManager draftManager
-			, IConnection connection
-			, MultiThreadedScheduler scheduler
+			, Context.AccessTuple accessTuple
 			, StandardLogger logger
+			, LongSupplier currentTimeMillisGenerator
+			, ExplicitCacheManager explicitCacheManager
 			, URL baseUrl
 	)
 	{
 		// We want to see if the key name we are working with is one which exists for us (could be null).
 		IpfsKey selectedHomeKey = null;
-		try (IReadOnlyLocalData reading = localDataModel.openForRead())
+		try (IReadOnlyLocalData reading = accessTuple.sharedDataModel().openForRead())
 		{
 			ChannelData channels = reading.readLocalIndex();
 			selectedHomeKey = channels.getPublicKey(keyName);
@@ -227,14 +228,15 @@ public class Cacophony {
 		
 		CacheUpdater cacheUpdater = new CacheUpdater(null, null, null, null, null);
 		Context context = new Context(draftManager
-				, new Context.AccessTuple(localDataModel, connection, scheduler)
-				, () -> System.currentTimeMillis()
+				, accessTuple
+				, currentTimeMillisGenerator
 				, logger
 				, baseUrl
 				, null
 				, null
 				, null
 				, cacheUpdater
+				, explicitCacheManager
 				, selectedHomeKey
 		);
 		return context;

src/com/jeffdisher/cacophony/DataDomain.java
@@ -135,6 +135,7 @@ public class DataDomain implements Closeable
 				, null
 				, new CacheUpdater(null, null, null, null, null)
 				, null
+				, null
 		);
 		
 		// Create the "them" user - this is the fake "remote" user
@@ -164,6 +165,7 @@ public class DataDomain implements Closeable
 				, null
 				, null
 				, new CacheUpdater(null, null, null, null, null)
+				, null
 				, null
 		);
 		new CreateChannelCommand(keyName).runInContext(theirContext);

src/com/jeffdisher/cacophony/commands/ListRecommendationsCommand.java
@@ -90,7 +90,7 @@ public record ListRecommendationsCommand(IpfsKey _targetKey) implements ICommand
 	{
 		context.logger.logVerbose("Check explicit cache: " + keyToCheck);
 		// Consult the cache - this never returns null but will throw on error.
-		ExplicitCacheData.UserInfo userInfo = context.getExplicitCache().loadUserInfo(keyToCheck).get();
+		ExplicitCacheData.UserInfo userInfo = context.explicitCacheManager.loadUserInfo(keyToCheck).get();
 		
 		AbstractRecommendations result;
 		try (IWritingAccess access = StandardAccess.writeAccess(context))

src/com/jeffdisher/cacophony/commands/PurgeExplicitCacheCommand.java
@@ -13,7 +13,7 @@ public record PurgeExplicitCacheCommand() implements ICommand<None>
 	@Override
 	public None runInContext(Context context) throws IpfsConnectionException
 	{
-		context.getExplicitCache().purgeCacheFullyAndGc().get();
+		context.explicitCacheManager.purgeCacheFullyAndGc().get();
 		return None.NONE;
 	}
 }

src/com/jeffdisher/cacophony/commands/ReadDescriptionCommand.java
@@ -117,7 +117,7 @@ public record ReadDescriptionCommand(IpfsKey _channelPublicKey) implements IComm
 	{
 		context.logger.logVerbose("Check explicit cache: " + keyToCheck);
 		// Consult the cache - this never returns null but will throw on error.
-		ExplicitCacheData.UserInfo userInfo = context.getExplicitCache().loadUserInfo(keyToCheck).get();
+		ExplicitCacheData.UserInfo userInfo = context.explicitCacheManager.loadUserInfo(keyToCheck).get();
 		ChannelDescription result;
 		try (IWritingAccess access = StandardAccess.writeAccess(context))
 		{

src/com/jeffdisher/cacophony/commands/ShowPostCommand.java
@@ -47,7 +47,7 @@ public record ShowPostCommand(IpfsFile _elementCid, boolean _forceCache) impleme
 		else if (!post.isKnownToBeCached)
 		{
 			// See if we can override this with a more concretely cached element from the explicit cache.
-			CachedRecordInfo existingInfo = context.getExplicitCache().getExistingRecord(_elementCid);
+			CachedRecordInfo existingInfo = context.explicitCacheManager.getExistingRecord(_elementCid);
 			// This can be null since we are just checking if it already has the info.
 			if (null != existingInfo)
 			{
@@ -118,7 +118,7 @@ public record ShowPostCommand(IpfsFile _elementCid, boolean _forceCache) impleme
 
 	private PostDetails _checkExplicitCache(Context context) throws ProtocolDataException, IpfsConnectionException, FailedDeserializationException
 	{
-		CachedRecordInfo info = context.getExplicitCache().loadRecord(_elementCid).get();
+		CachedRecordInfo info = context.explicitCacheManager.loadRecord(_elementCid).get();
 		try (IReadingAccess access = StandardAccess.readAccess(context))
 		{
 			// Everything in the explicit cache is cached.

src/com/jeffdisher/cacophony/interactive/GET_CacheStats.java
@@ -29,7 +29,7 @@ public class GET_CacheStats implements ValidatedEntryPoints.GET
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response, Object[] path) throws Throwable
 	{
-		long explicitCacheBytes = _context.getExplicitCache().getExplicitCacheSize();
+		long explicitCacheBytes = _context.explicitCacheManager.getExplicitCacheSize();
 		try (IReadingAccess access = StandardAccess.readAccess(_context))
 		{
 			long followeeCacheBytes = CacheHelpers.getCurrentCacheSizeBytes(access.readableFolloweeData());

commit 31c889a4ad06813775f2caa23b5c15e90719d6c3

Fixed bugs related to ExplicitCacheManager instance
-this fixes a bug introduced by 5106a246e440886213bcb43aefbc2415eb1df0b5, which results in the server using the initial single-threaded ExplicitCacheManager instead of the asynchronous one
-also fixes a bug introduced by 448de810e7f5efd2d6d4c93bcd9c2f6c8a31a98c, which results in the purge cache command having no effect and the common case never purging excess when the cache overflows

src/com/jeffdisher/cacophony/commands/Context.java
@@ -107,7 +107,7 @@ public class Context
 				, userInfoCache
 				, entryRegistry
 				, cacheUpdater
-				, this.explicitCacheManager
+				, explicitCacheManager
 				, _selectedKey
 		);
 		return context;

src/com/jeffdisher/cacophony/logic/ExplicitCacheManager.java
@@ -28,6 +28,7 @@ import com.jeffdisher.cacophony.types.KeyException;
 import com.jeffdisher.cacophony.types.ProtocolDataException;
 import com.jeffdisher.cacophony.types.SizeConstraintException;
 import com.jeffdisher.cacophony.utils.Assert;
+import com.jeffdisher.cacophony.utils.MiscHelpers;
 import com.jeffdisher.cacophony.utils.SizeLimits;
 
 
@@ -86,14 +87,14 @@ public class ExplicitCacheManager
 		_currentTimeMillisSupplier = currentTimeMillisSupplier;
 		if (enableAsync)
 		{
-			_background = new Thread(() -> {
+			_background = MiscHelpers.createThread(() -> {
 				Runnable runner = _backgroundGetNextRunnable();
 				while (null != runner)
 				{
 					runner.run();
 					runner = _backgroundGetNextRunnable();
 				}
-			});
+			}, "ExplicitCacheManager");
 			_userRequests = new HashMap<>();
 			_recordRequests = new HashMap<>();
 			_isBackgroundRunning = true;
@@ -706,7 +707,8 @@ public class ExplicitCacheManager
 			
 			if (null != purgeRequest)
 			{
-				_purgeExcess(access, data, explicitCacheTargetBytes);
+				// If we were given an explicit purge request, it actually means we need to clear the entire cache and complete the future.
+				_purgeExcess(access, data, 0L);
 				try
 				{
 					access.requestIpfsGc();
@@ -717,6 +719,11 @@ public class ExplicitCacheManager
 					purgeRequest.failure(e);
 				}
 				purgeRequest = null;
+			}
+			else
+			{
+				// If not, we just want to the basic purge of anything over limit.
+				_purgeExcess(access, data, explicitCacheTargetBytes);
 			}
 		}
 		