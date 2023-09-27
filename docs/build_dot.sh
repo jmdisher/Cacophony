c826216f77ea5db7fd27976c7ea6ec3715f1368d
Jeff Disher <jeff@jeffdisher.com>
2023-09-27 01:25:12 -0400

    Added an option for collapsing sub-packages
    -this is useful for removing the packages under data.local and data.global since they are largely self-contained and just clutter the diagram

--- docs/build_dot.sh
+++ docs/build_dot.sh
@@ -2,15 +2,26 @@
 
 # This script is just a tool to help with documentation and general insight into the shape of the codebase, but it is writting not to be too specific to this project.
 # When given a root source directory and root package name, it will walk all source in the directory and build a GraphViz DOT graph of the package relationships under the root package, writing the source to STDOUT.
+# If any additional arguments are provided, they are treated as packages to be collapsed.  This means that any sub-packages of these packages are treated as though they are in the top-level (useful for collapsing parts of the system which are internally organized, but their details are not relevant from the top-level).
 
 
-if [ $# -ne 2 ]; then
-        echo "Missing arguments: root_src_dir root_package"
+if [ $# -lt 2 ]; then
+        echo "Missing arguments: root_src_dir root_package [package_to_collapse]*"
         exit 1
 fi
 
 ROOT_DIR="$1"
 ROOT_PACKAGE="$2"
+
+# We want to build an array of all packages to collapse.
+COLLAPSE=()
+INDEX=2
+ARGS=($@)
+while [ $INDEX -lt $# ]; do
+	NAME=${ARGS[INDEX]}
+	COLLAPSE+=("$NAME")
+	INDEX=$((INDEX+1))
+done
 
 # We are searching for the package declaration and import lines.  We are assuming only a single space between.
 PACKAGE_PREFIX="package $ROOT_PACKAGE"
@@ -30,6 +41,13 @@ for FILE in `find "$ROOT_DIR" -name "*.java"`; do
 		SOURCE=`echo "$TOP" | cut -d . -f 2-`
 	fi
 	
+	# See if we want to collapse this one.
+	for CHECK in "${COLLAPSE[@]}"; do
+		if [[ "$SOURCE" == "$CHECK"* ]]; then
+			SOURCE="$CHECK"
+		fi
+	done
+	
 	# Walk all the import statements.
 	for SEGMENT in `grep "$IMPORT_PREFIX" $FILE | sed -e "s/^$IMPORT_PREFIX\.//" -e "s/;$//" | sort | uniq`; do
 		# Assume we want to get rid of the last name (since that would be the class - this assumes that we don't include inner classes, directly).
@@ -43,9 +61,18 @@ for FILE in `find "$ROOT_DIR" -name "*.java"`; do
 			TARGET=`echo "$SEGMENT" | cut -d . -f 1-$COUNT`
 		fi
 		
+		# See if we want to collapse this one.
+		for CHECK in "${COLLAPSE[@]}"; do
+			if [[ "$TARGET" == "$CHECK"* ]]; then
+				TARGET="$CHECK"
+			fi
+		done
+		
 		# Build the association we will be writing into the DOT source and store it in the associative array.
-		KEY="\"$SOURCE\" -> \"$TARGET\""
-		SET[${KEY}]="$KEY"
+		if [[ "$SOURCE" != "$TARGET" ]]; then
+			KEY="\"$SOURCE\" -> \"$TARGET\""
+			SET[${KEY}]="$KEY"
+		fi
 	done
 done
 
